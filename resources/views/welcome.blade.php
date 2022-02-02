a08f15129f91b61000c05e57510e4febedb5f52e
chinhvc <chinh.jawhm@gmail.com>
2022-02-02 19:28:01 +0900

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                     </svg>
                 </div>
 				<div>
-					<h1>CODE BUID SUCCESSFULL</h1>
+					<h1>CODE BUID SUCCESSFULL v1.1</h1>
 				</div>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
