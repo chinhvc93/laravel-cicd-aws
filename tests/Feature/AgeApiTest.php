c4075ab84218229651dda46384aecf6737945d44
chinhvc <chinh.jawhm@gmail.com>
2022-02-13 20:59:54 +0900

    update

--- tests/Feature/AgeApiTest.php
+++ tests/Feature/AgeApiTest.php
@@ -17,94 +17,94 @@ class AgeApiTest extends TestCase
     {
         $response = $this->postJson('/api/age', ['age' => -1]);
         $response->assertStatus(500)
-        ->assertJson(fn (AssertableJson $json) =>
-            $json->where('message', "ERROR")
-                 ->etc()
-        );
+        ->assertJson(function (AssertableJson $json) {
+            return $json->where('message', "ERROR")
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => 3]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "無料")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 5]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "無料")
-                 ->etc()
-        );
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => 6]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 10]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 12]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => 13]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,000円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 15]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,000円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 17]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,000円")
-                 ->etc()
-        );
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => 18]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 88]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => 120]);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "1,500円")
-                 ->etc()
-        );
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => 121]);
         $response->assertStatus(500)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "ERROR")
-                 ->etc()
-        );
+                 ->etc();
+        });
 
         $response = $this->postJson('/api/age', ['age' => '0001']);
         $response->assertStatus(200)
-        ->assertJson(fn (AssertableJson $json) =>
+        ->assertJson(function (AssertableJson $json)  {
             $json->where('message', "無料")
-                 ->etc()
-        );
+                 ->etc();
+        });
         $response = $this->postJson('/api/age', ['age' => '1+1']);
         $response->assertStatus(500);
     }
